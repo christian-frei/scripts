340fcaa3b7b92c9ba00f38bf5f8540cd8ec4de91
Christian Frei <christian.frei@gmail.com>
2021-04-14 21:11:45 +0200

    prometheus setup

--- setupPrometheus.sh
+++ setupPrometheus.sh
@@ -1,7 +1,13 @@
+#! /bin/bash
+
+cat > prometheus.yml <<-EOF
 global:
-  scrape_interval: 15
+  scrape_interval: 15s
 scrape_configs:
   - job_name: 'microservice'
     metrics_path: /q/metrics/application
     static_configs:
       - targets: ['localhost:8080']
+EOF
+
+exit 0
