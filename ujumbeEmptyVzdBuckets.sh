1fae8b3050af63257f11cec9c4eca41c82eb1bd1
Christian Frei <christian.frei@gmail.com>
2023-01-31 18:22:41 +0100

    ujumbe aws s3 bucket cleaner for VZD

---
+++ ujumbeEmptyVzdBuckets.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+aws s3 rm s3://ujumbe-vzd-contacts --recursive
+aws s3 rm s3://ujumbe-vzd-domains --recursive
+aws s3 rm s3://ujumbe-vzd-federation-lists --recursive
+aws s3 rm s3://ujumbe-vzd-fhir-directory --recursive
+aws s3 rm s3://ujumbe-vzd-identification --recursive
